ebb63d6f048d5c7770e3703b095ec3e2f0f9cf6c
Charles Aspegren <charles@berrystreetconsulting.co>
2025-12-14 13:55:16 -0500

    Install dependencies and update cron job script

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
 FROM python:3.12-slim
+
 WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
 COPY . .
+
 CMD ["python", "jobs/cron/main.py"]
--- jobs/cron/main.py
+++ jobs/cron/main.py
@@ -1,6 +1,36 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
+import requests
+import secrets
 
-now = datetime.now(tz=ZoneInfo("UTC"))
-print("UTC:", now.isoformat())
-print("ET :", now.astimezone(ZoneInfo("America/New_York")).isoformat())
+def generate_unique_id():
+    return secrets.randbelow(9000000000) + 1000000000
+
+unique_id = generate_unique_id()
+
+##project_id = 'digest-a3589-default-rtdb'
+
+post_url = f'https://digest-a3589-default-rtdb.firebaseio.com/{unique_id}.json'
+
+current_date = (datetime.now() - timedelta(days=1)).strftime("%Y_%B_%-d")
+category = "News"
+title = "Current Events Summary"
+link = f'https://en.wikipedia.org/wiki/Portal:Current_events/{current_date}'
+summary = "n/a"
+date = "n/a"
+source = "n/a"
+metadata = "n/a"
+    
+payload = {
+    "category": category,
+    "title": title,
+    "metadata": metadata,
+    "link": link,
+    "summary": summary,
+    "source": source,
+    "date": date
+    }
+
+response = requests.post(post_url, json=payload)
+response.raise_for_status()
+print(f"Successfully posted payload: {response.status_code}")
