0a9e226657d1a5989ef9331c5fab14f83a4d87d3
Charles Aspegren <charles@berrystreetconsulting.co>
2025-12-19 19:25:27 -0500

    Update RSS feeds

--- jobs/cron/rss_feeds.py
+++ jobs/cron/rss_feeds.py
@@ -1,3 +1,7 @@
 FEED_URLS=[
-  "https://www.obsolete.pub/feed"
+  "https://www.obsolete.pub/feed",
+  "https://blog.eladgil.com/feed",
+  "https://newsletter.posthog.com/feed",
+  "https://read.highgrowthengineer.com/feed",
+  "https://longform.asmartbear.com/index.xml"
 ]
